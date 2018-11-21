1a03b8b1d0817117806e7f1b500ce0227c7330f0
abdi12u <karim.abdi9@etu.univ-lorraine.fr>
2018-11-21 11:23:22 +0100

    test

--- GuessWhat/utilisateur.cpp
+++ GuessWhat/utilisateur.cpp
@@ -1,5 +1,33 @@
-#include "utilisateur.h"
+#include "Utilisateur.h"
 
-Utilisateur::Utilisateur()
-{
+//test
+
+void Utilisateur::selectionnerPaquet(int idPaquet){
+
 }
+
+std::string  Utilisateur::creerCarte(std::string  Recto , std::string  Verso){
+
+}
+
+std::string Utilisateur::creerPaquet(std::string  nom , std::string  categorie){
+
+}
+
+void Utilisateur::afficherVerso(){
+
+}
+
+void Utilisateur::afficherRecto(){
+
+}
+
+void Utilisateur::evaluerCarte(int note , int idUtilisateur){
+
+}
+
+
+void Utilisateur::partagerPaquet(){
+
+}
+
